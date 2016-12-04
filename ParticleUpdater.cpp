94dd3936d1c125c11f8d8bc757dd6f6329814254
amdreallyfast <amdreallyfast@hotmail.com>
2016-12-04 09:29:24 -0700

    finished doing neighbor setup (that took longer than expected)

--- ParticleUpdater.cpp
+++ ParticleUpdater.cpp
@@ -8,28 +8,37 @@ struct QuadTreeNode
         _numCurrentParticles(0),
         _startingParticleIndex(0),
         _inUse(0),
+        _isSubdivided(0),
+        _childNodeIndexTopLeft(-1),
+        _childNodeIndexTopRight(-1),
+        _childNodeIndexBottomRight(-1),
+        _childNodeIndexBottomLeft(-1),
         _leftEdge(0.0f),
         _topEdge(0.0f),
         _rightEdge(0.0f),
         _bottomEdge(0.0f),
         _neighborIndexLeft(-1),
-        _neighborIndexUpLeft(-1),
-        _neighborIndexUp(-1),
-        _neighborIndexUpright(-1),
+        _neighborIndexTopLeft(-1),
+        _neighborIndexTop(-1),
+        _neighborIndexTopRight(-1),
         _neighborIndexRight(-1),
-        _neighborIndexDownRight(-1),
-        _neighborIndexDown(-1),
-        _neighborIndexDownLeft(-1)
+        _neighborIndexBottomRight(-1),
+        _neighborIndexBottom(-1),
+        _neighborIndexBottomLeft(-1)
     {
         memset(_indicesForContainedParticles, 0, sizeof(int) * MAX_PARTICLES_PER_QUAD_TREE_NODE);
     }
 
     int _indicesForContainedParticles[MAX_PARTICLES_PER_QUAD_TREE_NODE];
     int _numCurrentParticles;
-
-    // for use in GPU version
     int _startingParticleIndex;
+
     int _inUse;
+    int _isSubdivided;
+    int _childNodeIndexTopLeft;
+    int _childNodeIndexTopRight;
+    int _childNodeIndexBottomRight;
+    int _childNodeIndexBottomLeft;
 
     // left and right edges implicitly X, top and bottom implicitly Y
     float _leftEdge;
@@ -38,22 +47,24 @@ struct QuadTreeNode
     float _bottomEdge;
 
     int _neighborIndexLeft;
-    int _neighborIndexUpLeft;
-    int _neighborIndexUp;
-    int _neighborIndexUpright;
+    int _neighborIndexTopLeft;
+    int _neighborIndexTop;
+    int _neighborIndexTopRight;
     int _neighborIndexRight;
-    int _neighborIndexDownRight;
-    int _neighborIndexDown;
-    int _neighborIndexDownLeft;
+    int _neighborIndexBottomRight;
+    int _neighborIndexBottom;
+    int _neighborIndexBottomLeft;
 
 };
 
 // increase the number of additional nodes as necessary to handle more subdivision
+// Note: This algorithm was built with the compute shader's implementation in mind.  These structures are meant to be used as if a compute shader was running it, hence all the arrays and a complete lack of runtime memory reallocation.
 const int NUM_ROWS_IN_TREE_INITIAL = 8;
 const int NUM_COLUMNS_IN_TREE_INITIAL = 8;
 const int NUM_STARTING_NODES = NUM_ROWS_IN_TREE_INITIAL * NUM_COLUMNS_IN_TREE_INITIAL;
 const int MAX_NODES = NUM_STARTING_NODES * 4;
 QuadTreeNode allQuadTreeNodes[MAX_NODES];
+int numNodesInUse = NUM_STARTING_NODES;
 
 // TODO: On GPU version, start with an 8x8 array of nodes and have a work item operating on each possible node
 
@@ -68,11 +79,177 @@ void InitializeTree(const glm::vec2 &particleRegionCenter, float particleRegionR
     float x = particleRegionCenter.x - particleRegionRadius;
     float y = particleRegionCenter.y + particleRegionRadius;
 
-    for (int nodeIndex = 0; nodeIndex < NUM_STARTING_NODES; nodeIndex++)
+    for (int row = 0; row < NUM_ROWS_IN_TREE_INITIAL; row++)
     {
-        QuadTreeNode &nodeRef = allQuadTreeNodes[nodeIndex];
+        for (int column = 0; column < NUM_COLUMNS_IN_TREE_INITIAL; column++)
+        {
+            int nodeIndex = (row * NUM_COLUMNS_IN_TREE_INITIAL) + column;
+            QuadTreeNode &node = allQuadTreeNodes[nodeIndex];
 
+            // set the borders of the node
+            node._leftEdge = x;
+            node._rightEdge = x + xIncrement;
+            node._topEdge = y;
+            node._bottomEdge = y - yIncrement;
+
+            // assign neighbors
+            // Note: Nodes on the edge of the initial tree have three null neighbors (ex: a top-row node has null top left, top, and top right neighbors).  There may be other ways to calculate these, but I chose the following because it is spelled out verbosely.
+
+            // left edge does not have a "left" neighbor
+            if (column > 0)
+            {
+                node._neighborIndexLeft = nodeIndex - 1;
+            }
+
+            // left and top edges do not have a "top left" neighbor
+            if (row > 0 && column > 0)
+            {
+                // "top left" neighbor = current node - 1 row - 1 column
+                node._neighborIndexTopLeft = nodeIndex - NUM_COLUMNS_IN_TREE_INITIAL - 1;
+            }
+
+            // top row does not have a "top" neighbor
+            if (row > 0)
+            {
+                node._neighborIndexTop = nodeIndex - NUM_COLUMNS_IN_TREE_INITIAL;
+            }
+
+            // right and top edges do not have a "top right" neighbor
+            if (row > 0 && column < (NUM_COLUMNS_IN_TREE_INITIAL - 1))
+            {
+                // "top right" neighbor = current node - 1 row + 1 column
+                node._neighborIndexTopRight = nodeIndex - NUM_COLUMNS_IN_TREE_INITIAL + 1;
+            }
+
+            // right edge does not have a "right" neighbor
+            if (column < (NUM_COLUMNS_IN_TREE_INITIAL - 1))
+            {
+                node._neighborIndexRight = nodeIndex + 1;
+            }
+
+            // right and bottom edges do not have a "bottom right" neighbor
+            if (row < (NUM_ROWS_IN_TREE_INITIAL - 1) &&
+                column < (NUM_COLUMNS_IN_TREE_INITIAL - 1))
+            {
+                // "bottom right" neighbor = current node + 1 row + 1 column
+                node._neighborIndexBottomRight = nodeIndex + NUM_COLUMNS_IN_TREE_INITIAL + 1;
+            }
+
+            // bottom edge does not have a "bottom" neighbor
+            if (row < (NUM_ROWS_IN_TREE_INITIAL - 1))
+            {
+                node._neighborIndexBottom = nodeIndex + NUM_COLUMNS_IN_TREE_INITIAL;
+            }
+
+            // left and bottom edges do not have a "bottom left" neighbor
+            if (row < (NUM_ROWS_IN_TREE_INITIAL - 1) && column > 0)
+            {
+                // bottom left neighbor = current node + 1 row - 1 column
+                node._neighborIndexBottomLeft = nodeIndex + NUM_COLUMNS_IN_TREE_INITIAL - 1;
+            }
+
+
+            // setup for next node
+            x += xIncrement;
+            y -= yIncrement;
+        }
     }
+
+}
+
+// TODO: header
+void ResetTree()
+{
+    for (int nodeIndex = 0; nodeIndex < MAX_NODES; nodeIndex++)
+    {
+        QuadTreeNode &node = allQuadTreeNodes[nodeIndex];
+        node._numCurrentParticles = 0;
+        node._startingParticleIndex = 0;
+        node._isSubdivided = 0;
+        node._childNodeIndexTopLeft = -1;
+        node._childNodeIndexTopRight = -1;
+        node._childNodeIndexBottomRight = -1;
+        node._childNodeIndexBottomLeft = -1;
+
+        // all excess nodes are turned off
+        if (nodeIndex > NUM_STARTING_NODES)
+        {
+            node._inUse = false;
+        }
+    }
+
+    numNodesInUse = NUM_STARTING_NODES;
+}
+
+// TODO: header
+void SubdivideNode(int nodeIndex)
+{
+    QuadTreeNode &node = allQuadTreeNodes[nodeIndex];
+    node._isSubdivided = 1;
+
+    int childNodeIndexTopLeft = numNodesInUse++;
+    int childNodeIndexTopRight = numNodesInUse++;
+    int childNodeIndexBottomRight = numNodesInUse++;
+    int childNodeIndexBottomLeft = numNodesInUse++;
+
+    if (numNodesInUse > MAX_NODES)
+    {
+        fprintf(stderr, "number nodes in use '%d' exceeds max nodes '%d'\n", numNodesInUse, MAX_NODES);
+    }
+
+    node._childNodeIndexTopLeft = childNodeIndexTopLeft;
+    node._childNodeIndexTopRight = childNodeIndexTopRight;
+    node._childNodeIndexBottomRight = childNodeIndexBottomRight;
+    node._childNodeIndexBottomLeft = childNodeIndexBottomLeft;
+
+    // assign neighbors
+    // Note: This is going to get really messy when I move to 3D and have to use octrees, each 
+    // with 26 neighbors ((3 * 3 * 3) - 1 center node).
+
+    QuadTreeNode &childTopLeft = allQuadTreeNodes[childNodeIndexTopLeft];
+    childTopLeft._neighborIndexLeft = node._neighborIndexLeft;
+    childTopLeft._neighborIndexTopLeft = node._neighborIndexTopLeft;
+    childTopLeft._neighborIndexTop = node._neighborIndexTop;
+    childTopLeft._neighborIndexTopRight = node._neighborIndexTop;
+    childTopLeft._neighborIndexRight = childNodeIndexTopRight;
+    childTopLeft._neighborIndexBottomRight = childNodeIndexBottomRight;
+    childTopLeft._neighborIndexBottom = childNodeIndexBottomLeft;
+    childTopLeft._neighborIndexBottomLeft = node._neighborIndexLeft;
+    childTopLeft._startingParticleIndex = node._indicesForContainedParticles[MAX_PARTICLES_PER_QUAD_TREE_NODE - 1];
+
+    QuadTreeNode &childTopRight = allQuadTreeNodes[childNodeIndexTopRight];
+    childTopRight._neighborIndexLeft = childNodeIndexTopLeft;
+    childTopRight._neighborIndexTopLeft = node._neighborIndexTop;
+    childTopRight._neighborIndexTop = node._neighborIndexTop;
+    childTopRight._neighborIndexTopRight = node._neighborIndexTopRight;
+    childTopRight._neighborIndexRight = node._neighborIndexRight;
+    childTopRight._neighborIndexBottomRight = node._neighborIndexRight;
+    childTopRight._neighborIndexBottom = childNodeIndexBottomRight;
+    childTopRight._neighborIndexBottomLeft = childNodeIndexBottomLeft;
+    childTopLeft._startingParticleIndex = node._indicesForContainedParticles[MAX_PARTICLES_PER_QUAD_TREE_NODE - 1];
+
+    QuadTreeNode &childBottomRight = allQuadTreeNodes[childNodeIndexBottomRight];
+    childBottomRight._neighborIndexLeft = childNodeIndexBottomLeft;
+    childBottomRight._neighborIndexTopLeft = childNodeIndexTopLeft;
+    childBottomRight._neighborIndexTop = childNodeIndexTopRight;
+    childBottomRight._neighborIndexTopRight = node._neighborIndexRight;
+    childBottomRight._neighborIndexRight = node._neighborIndexRight;
+    childBottomRight._neighborIndexBottomRight = node._neighborIndexBottomRight;
+    childBottomRight._neighborIndexBottom = node._neighborIndexBottom;
+    childBottomRight._neighborIndexBottomLeft = node._neighborIndexBottom;
+    childTopLeft._startingParticleIndex = node._indicesForContainedParticles[MAX_PARTICLES_PER_QUAD_TREE_NODE - 1];
+
+    QuadTreeNode &childBottomLeft = allQuadTreeNodes[childNodeIndexBottomLeft];
+    childBottomLeft._neighborIndexLeft = node._neighborIndexLeft;
+    childBottomLeft._neighborIndexTopLeft = node._neighborIndexLeft;
+    childBottomLeft._neighborIndexTop = childNodeIndexTopLeft;
+    childBottomLeft._neighborIndexTopRight = childNodeIndexTopRight;
+    childBottomLeft._neighborIndexRight = childNodeIndexBottomRight;
+    childBottomLeft._neighborIndexBottomRight = node._neighborIndexBottom;
+    childBottomLeft._neighborIndexBottom = node._neighborIndexBottom;
+    childBottomLeft._neighborIndexBottomLeft = node._neighborIndexBottomLeft;
+    childTopLeft._startingParticleIndex = node._indicesForContainedParticles[MAX_PARTICLES_PER_QUAD_TREE_NODE - 1];
+
 }
 
 // TODO: header
